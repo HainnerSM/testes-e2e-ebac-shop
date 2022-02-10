63a8fa9b91f54e3e802771d6598078216da1801c
Hainner Molmelstet <hainner.sm@gmail.com>
2022-02-10 10:39:02 -0300

    adicionando pipeline jenkins

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -31,6 +31,7 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
         //TODO 
         cy.get('#primary-menu > .menu-item-629 > a').click()
+        //funcao para escolha do produto pelo nome,tamanho e cor
         cy.addProduto('Aero Daily Fitness Tee', 'S', 'Brown', 1)
 
         cy.get('#primary-menu > .menu-item-629 > a').click()
@@ -42,22 +43,18 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         cy.get('#primary-menu > .menu-item-629 > a').click()
         cy.addProduto('Atlas Fitness Tank', 'M', 'Blue', 2)
         cy.get('.checkout-button').click()
+
+        //funcao para preenchimento do checkout
         faturamento.editarFaturamento('Hainner', 'Molmelstet', 'lightaction', 'Brasil', 'Rua de la', 655,
             'Votorantim', 'São Paulo', 18116050, 32222228, 'hainner@email.com')
 
         cy.get('#payment_method_cod').click()
         cy.get('#terms').click()
         cy.get('#place_order').click()
+        //validando a compra
         cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
 
-
-
-
-
-
-
-
-    });
+     });
 
 
 })
